18e89a75247c55670f3aff6faf530ef9f3f2c1b9
ahfasxp <ahfas.fazria@gmail.com>
2020-11-28 06:08:31 +0700

    adapter dan setup recycleview

--- app/src/main/java/com/distin/placeit/ui/restaurant/RestaurantAdapter.kt
+++ app/src/main/java/com/distin/placeit/ui/restaurant/RestaurantAdapter.kt
@@ -1,4 +1,60 @@
 package com.distin.placeit.ui.restaurant
 
-class RestaurantAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.distin.placeit.R
+import com.distin.placeit.core.data.response.RestaurantResponse
+import kotlinx.android.synthetic.main.item_list.view.*
+
+class RestaurantAdapter : RecyclerView.Adapter<RestaurantAdapter.RestaurantViewHolder>() {
+    private var listRestaurants = ArrayList<RestaurantResponse>()
+    var onItemClick: ((RestaurantResponse) -> Unit)? = null
+
+    fun setData(restaurant: List<RestaurantResponse>?) {
+        if (restaurant == null) return
+        listRestaurants.clear()
+        listRestaurants.addAll(restaurant)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RestaurantViewHolder {
+        val mView = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
+        return RestaurantViewHolder(mView)
+    }
+
+    override fun onBindViewHolder(holder: RestaurantViewHolder, position: Int) {
+        val restaurant = listRestaurants[position]
+        holder.bind(restaurant)
+    }
+
+    override fun getItemCount(): Int = listRestaurants.size
+
+    inner class RestaurantViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(restaurant: RestaurantResponse) {
+            with(itemView) {
+                Glide.with(context)
+                    .load(restaurant.name)
+                    .apply(
+                        RequestOptions.placeholderOf(R.drawable.ic_loading)
+                            .error(R.drawable.ic_error)
+                    )
+                    .into(img_photo)
+                tv_title.text = restaurant.name
+                tv_rate.text = restaurant.placeId
+                tv_description.text = restaurant.placeId
+                tv_distance.text = restaurant.placeId
+
+                itemView.setOnClickListener {
+                    onItemClick?.invoke(listRestaurants[adapterPosition])
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/distin/placeit/ui/restaurant/RestaurantFragment.kt
+++ app/src/main/java/com/distin/placeit/ui/restaurant/RestaurantFragment.kt
@@ -1,6 +1,7 @@
 package com.distin.placeit.ui.restaurant
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
@@ -9,9 +10,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.distin.placeit.R
 import com.distin.placeit.utils.ext.observe
 import com.google.android.gms.location.LocationServices
+import kotlinx.android.synthetic.main.fragment_restaurant.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 
@@ -29,9 +33,22 @@ class RestaurantFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val restaurantAdapter = RestaurantAdapter()
+
         getLocation()
         observe(viewModel.getRestaurant()) {
             Log.d("cekdata", it.toString())
+            restaurantAdapter.setData(it)
+        }
+
+        with(rv_restaurant) {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = restaurantAdapter
+        }
+        restaurantAdapter.onItemClick = { selectedData ->
+
         }
     }
 
